eedac5eb7492d51dc7f5820c346dadd77755729d
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2024-12-21 21:34:52 +0400

    *

--- .ipynb_checkpoints/info-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("data.csv")
-print(data.info())
-print(data.head())
--- notebooks/.ipynb_checkpoints/upload-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data_path = "data/data.csv"
-df = pd.read_csv(data_path)
-
-print(df.head())
